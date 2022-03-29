664527d816cec20f6a7e5e0184947421bb504774
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-03-29 09:29:38 +0500

    Quiz

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  List<int> marks = [10, 20, 30, 40, 50];
+  //var marks = [10, 20, 30, 40, 50];
+  // print(marks[0]);
+  // print(marks[1]);
+  // print(marks[2]);
+  // print(marks[3]);
+  // print(marks[4]);
+  // marks[0] = 120;
+  // print(marks[0]);
+
+  for (int i = 0; i <= 4; i++) {
+    print(marks[i]);
+  }
+}
